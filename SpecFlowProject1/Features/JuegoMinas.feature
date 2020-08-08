a05daad40aab20b6236a9221616a0829d3c4fd3b
Parterdev <litemeilateran@outlook.com>
2020-08-07 22:01:11 -0500

    Upload BDD to Minesweeper program with Selenium

---
+++ SpecFlowProject1/Features/JuegoMinas.feature
@@ -0,0 +1,11 @@
+﻿# language: es
+Característica: Buscaminas juego
+	Como jugador 
+	Quiero registrar mi nombre
+	Para asociarlo a mi puntaje
+
+@mytag
+Escenario: Ingreso
+	Dado que ingreso al sitio web
+	Cuando escribo mi nombre de usuario "Pepito"
+	Entonces el juego empieza
---
+++ SpecFlowProject1/Steps/BuscaminasJuegoSteps.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.CommonModels;
+
+namespace SpecFlowProject1.Steps
+{
+    [Binding]
+    public class BuscaminasJuegoSteps
+    {
+        //Implementacion del webdriver para firefox
+        IWebDriver driver = new FirefoxDriver();
+
+
+        [Given(@"que ingreso al sitio web")]
+        public void DadoQueIngresoAlSitioWeb()
+        {
+
+            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"C:\Selenium", "geckodriver.exe");
+            service.FirefoxBinaryPath = @"C:\Program Files\Mozilla Firefox\firefox.exe";
+            driver = new FirefoxDriver(service);
+
+            //App route
+            driver.Url = "https://localhost:8081/";
+            //Ampliamos la venta del navegador
+            driver.Manage().Window.Maximize();
+            driver.Navigate();
+        }
+        
+        [When(@"escribo mi nombre de usuario ""(.*)""")]
+        public void CuandoRegistroMi(string nombre)
+        {
+            driver.Url = "https://localhost:8081/login/index";
+            //Ampliamos la venta del navegador
+            driver.Manage().Window.Maximize();
+            driver.Navigate();
+            driver.FindElement(By.Name("username")).SendKeys(nombre);
+            driver.FindElement(By.Name("username")).SendKeys(nombre + Keys.Enter);
+            //driver.FindElement(By.Name("aceptar")).Click();
+        }
+        
+        [Then(@"el juego empieza")]
+        public void EntoncesElJuegoEmpieza()
+        {
+            var dato = driver.FindElement(By.Name("empezar")).GetAttribute("username");
+            var esperado = driver.FindElement(By.Name("empezar")).GetAttribute("username").Equals("Pepito");
+
+            //Assert.AreEqual(dato, esperado);
+            bool result = dato.Equals(esperado);
+
+            if (result){
+                Console.Out.Write("Test aprobado");
+                driver.Close();
+            }
+            else {
+                Console.Out.Write("El test ha fallado");
+                driver.Close();
+            }
+        }
+    }
+}
